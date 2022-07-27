792e1d21fd44820589fd0a3b64ab92c1dcffed8b
totorato <41010290+totorato@users.noreply.github.com>
2022-07-28 00:31:49 +0800

    allow all hosts

--- SoundLib/settings.py
+++ SoundLib/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6!yun@52j*$^#v9tk7qh1378x=b-e6lb$71#h^h9ke#4nu^&$1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
